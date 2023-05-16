7ab8faa9518f20e9494714781ba7a3ec0628300f
Juan Diego Marin Herrera <94065036+JuanM-25@users.noreply.github.com>
2023-05-15 19:56:39 -0600

    Primer commit del proyecto TiendaDAWP

---
+++ src/main/java/com/tiendaDAWP/tiendaDAWP/TiendaDawpApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaDAWP.tiendaDAWP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDawpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDawpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaDAWP/tiendaDAWP/TiendaDawpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaDAWP.tiendaDAWP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDawpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
